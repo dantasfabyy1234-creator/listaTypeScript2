084050420dc7ae68382f1f6c4d424b10c21b0ba7
josevitor21122-byte <josevitor21122@gmail.com>
2026-04-17 10:27:21 -0300

    aprendendo typescript
    aula de hoje

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+import { runQuestao2Condicional } from "./condicional/exercicio2.js";
+document.getElementById("btn2-condicional")?.addEventListener('click', runQuestao2Condicional);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello World")
+import { runQuestao2Condicional } from "./condicional/exercicio2.js";
+
+document.getElementById("btn2-condicional")?.addEventListener('click', runQuestao2Condicional)
